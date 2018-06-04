ec7f9128e74345d2661bf10956de809681329746
Luiz Galiza <lfdgaliza@gmail.com>
2018-06-04 18:12:07 +1000

    Including libs and initializing the appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private _router: Router) {   }
+
+  isActive(path: string) {
+    return this._router.url.endsWith(path);
+  }
 }
