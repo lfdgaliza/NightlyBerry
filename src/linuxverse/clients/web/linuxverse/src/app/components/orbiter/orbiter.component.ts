58a6c0a4ab4f08b042c10e67ca6455812b1b819a
Luiz Galiza <lfdgaliza@gmail.com>
2019-01-26 22:22:55 +0000

    resizing ok

--- src/linuxverse/clients/web/linuxverse/src/app/components/orbiter/orbiter.component.ts
+++ src/linuxverse/clients/web/linuxverse/src/app/components/orbiter/orbiter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { Orbiter } from 'src/app/services/models/orbiter';
 
 @Component({
@@ -6,12 +6,30 @@ import { Orbiter } from 'src/app/services/models/orbiter';
   templateUrl: './orbiter.component.html',
   styleUrls: ['./orbiter.component.scss']
 })
-export class OrbiterComponent implements OnInit {
-
+export class OrbiterComponent {
   @Input() orbiter: Orbiter
-  constructor() { }
+  @Input() position: number
+  @Input() depth: number
 
-  ngOnInit() {
+  constructor(private el: ElementRef) {
+    this.depth = this.depth || 0
   }
 
+  ngAfterViewInit() {
+    const orbiterElement = this.el.nativeElement.firstChild
+    const parentOrbiter = orbiterElement.parentElement.parentElement
+
+    this.resizeOrbiter(parentOrbiter, orbiterElement);
+
+
+  }
+
+
+  private resizeOrbiter(parentOrbiter: any, orbiterElement: any) {
+    if (this.depth > 0) {
+      const newSize = parentOrbiter.clientHeight / (this.depth + 1);
+      orbiterElement.style.height = `${newSize}px`;
+      orbiterElement.style.width = `${newSize}px`;
+    }
+  }
 }
