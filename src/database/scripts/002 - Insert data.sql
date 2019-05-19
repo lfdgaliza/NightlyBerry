8993dbdd2ce2ee9886e0f79005755f8cf4158402
Luiz Galiza <lfdgaliza@gmail.com>
2019-05-19 12:14:22 +0100

    Updates the intertion script

--- src/database/scripts/002 - Insert data.sql
+++ src/database/scripts/002 - Insert data.sql
@@ -1,6 +1,6 @@
-INSERT INTO `distro-guide`.`Translation` (ResourceName,Description,`Language`,`Translation`) VALUES 
-('DistroDetail:Intro',NULL,'en','Here is what we know about <distro> so far...')
-,('DistroDetail:Intro',NULL,'pt','Isso é o que sabemos sobre <distro> até então...')
-,('MainSearch:TryPlaceholder',NULL,'en','Try "<distro>"...')
-,('MainSearch:TryPlaceholder',NULL,'pt','Tente "<distro>"...')
+INSERT INTO `distro-guide`.`Translation` (Module,ResourceName,Description,`Language`,`Translation`) VALUES 
+('DistroDetail','Intro',NULL,'en','Here is what we know about <distro> so far...')
+,('DistroDetail','Intro',NULL,'pt','Isso é o que sabemos sobre <distro> até então...')
+,('MainSearch','TryPlaceholder',NULL,'en','Try "<distro>"...')
+,('MainSearch','TryPlaceholder',NULL,'pt','Tente "<distro>"...')
 ;
